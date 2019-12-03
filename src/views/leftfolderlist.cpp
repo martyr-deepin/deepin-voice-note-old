56e6dbd6d9053e67c496ca58c197f5c300c6ef5a
Liuda <liuda@smartens-tek.com>
2019-12-03 15:04:30 +0800

    change the width for leftlist

--- src/views/leftfolderlist.cpp
+++ src/views/leftfolderlist.cpp
@@ -31,7 +31,7 @@ void LeftFolderList::initUI()
     //this->setFocusPolicy(Qt::NoFocus);
     //this->setFocus(Qt::NoFocusReason);
     //this->setAttribute(Qt::WA_MacShowFocusRect, 0);
-    this->setFixedWidth(250);
+    this->setFixedWidth(248);
     //this->move(-10,this->y());
     m_contextMenu = new DMenu;
     m_renameAction = new QAction(tr("Rename"),this);
